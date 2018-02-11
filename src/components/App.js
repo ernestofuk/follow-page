54fe689495d533ffe6a377219469d25ffd4f8665
Ernesto <eafuk@synopsis.ws>
2018-02-10 20:43:19 -0500

    app

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
+import React, { Component } from 'react';
+import { Button, Glyphicon } from 'react-bootstrap'
 import './statics/css/bootstrap.css';
 
-const store = createStore(reducer)
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+class App extends Component {
+    render() {
+        return (
+            <div className="App" >
+                <Button bsStyle="danger"> <Glyphicon glyph="star" /> Test </Button>
+            </div>
+                );
+    }
+}
+
+export default App;
